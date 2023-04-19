ae7edde00dff45d5de1bc1df206283aef1075d71
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-19 14:29:25 +0600

    Starting with private variables and constructor

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,3 +1,17 @@
-public class MyArrayList {
+public class MyArrayList<T> {
+    private Object[] list;
+    private int size;
+    private final int DEFAULT_CAPACITY = 10;
 
+    public MyArrayList(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity <= 0");
+        } else {
+            list = (T[]) new Object[capacity];
+        }
+    }
+
+    public MyArrayList() {
+        list = (T[]) new Object[DEFAULT_CAPACITY];
+    }
 }
